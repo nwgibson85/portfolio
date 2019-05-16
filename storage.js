012cbef35c29fbaadff1009473b0fbb9819fdde3
Nicholas Gibson <Nwgibson85@gmail.com>
2019-05-16 11:03:13 -0500

    initial commit

---
+++ storage.js
@@ -0,0 +1,16 @@
+const myProjectsArr = [
+	
+    {title: "Random Animal Facts Quiz",
+    description: "This is my first ever from scratch project. Ten multiple choice questions randomly selected from a bank of twenty plus such questions. Through use of JQuery and DOM manipulation one can navigate this quiz start to finish and receive feedback on each answer.",
+    techsUsed: "HTML, CSS, JavaScript",
+    liveApp: "https://nwgibson85.github.io/quizApp/",
+      repo: "https://github.com/nwgibson85/quizApp",
+    displayImg: 'https://i.imgur.com/FW4QB3k.png'},
+]
+  
+//{title: "",
+//description: "",
+//techsUsed: "",
+//liveApp: "",
+//repo: ""}
+//]
