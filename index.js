e73af537af5f2cb9737211c2a3a6b0556418124d
Nicholas Gibson <Nwgibson85@gmail.com>
2019-05-22 11:22:00 -0500

    working on projects page

---
+++ index.js
@@ -0,0 +1,74 @@
+
+
+// display pages //
+function displayBio() {
+    $('#myWork').hide();
+    $('#contactMe').hide();
+    $('#resume').hide();
+    $('#bio').show();
+    $('#super').show();
+    
+}
+
+function displayMyWork() {
+    $('#myWork').show();
+    $('#contactMe').hide();
+    $('#resume').hide();
+    $('#bio').hide();
+    $('#super').hide();
+}
+
+function displayContactMe() {
+    $('#myWork').hide();
+    $('#contactMe').show();
+    $('#resume').hide();
+    $('#bio').hide();
+    $('#super').hide();
+}
+
+function displayResume() {
+    $('#myWork').hide();
+    $('#contactMe').hide();
+    $('#resume').show();
+    $('#bio').hide();
+    $('#super').hide();
+}
+
+// event handlers //
+function handleNickClick() {
+    $('.navButtons').on('click', '#Nick', function(event) {
+        event.preventDefault();
+        displayBio();
+    });
+}
+
+function handleWorkClick() {
+    $('.navButtons').on('click', '#Work', function(event) {
+        event.preventDefault();
+        displayMyWork();
+    });
+}
+
+function handleAboutMeClick() {
+    $('.navButtons').on('click', '#Work', function(event) {
+        event.preventDefault();
+        displayContactMe();
+    });
+}
+
+function handleResumeClick() {
+    $('').on('click', '#Work', function(event) {
+        event.preventDefault();
+    });
+}
+
+// start it up //
+function showMeOff() {
+    handleNickClick();
+    handleWorkClick();
+    handleAboutMeClick();
+    handleResumeClick();
+    displayBio();
+}
+
+$(showMeOff);
--- storage.js
+++ storage.js
@@ -4,7 +4,7 @@ const myProjectsArr = [
     description: "This is my first ever from scratch project. Ten multiple choice questions randomly selected from a bank of twenty plus such questions. Through use of JQuery and DOM manipulation one can navigate this quiz start to finish and receive feedback on each answer.",
     techsUsed: "HTML, CSS, JavaScript",
     liveApp: "https://nwgibson85.github.io/quizApp/",
-      repo: "https://github.com/nwgibson85/quizApp",
+    repo: "https://github.com/nwgibson85/quizApp",
     displayImg: 'https://i.imgur.com/FW4QB3k.png'},
 ]
   
